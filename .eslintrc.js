afebf84cef9baeb7b56723cc0c248d7b446a9516
Jackie098 <carlosmiranda19122@gmail.com>
2019-12-20 17:59:33 -0300

    Reconfigurando ESlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,10 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
+    'prettier'
+  ],
+  plugins:[
+    'prettier'
   ],
   globals: {
     Atomics: 'readonly',
@@ -15,5 +19,10 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camalcase": "off",
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next"}]
   },
 };
