d3cf48c6bebec09034f1441bb44009a683de56c2
Jackie098 <carlosmiranda19122@gmail.com>
2019-12-23 23:47:49 -0300

    Criado model de usuário

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ welcome: 'Hello motherfucks' }));
+
+Router.post('/users', UserController.store);
 
 export default routes;
