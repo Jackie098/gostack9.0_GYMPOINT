1e0d6061ce4963d81213dfa76832efd05d28e651
Jackie098 <carlosmiranda19122@gmail.com>
2019-12-25 18:38:38 -0300

    Corrigindo a tabela de usuários adm

--- src/database/seeds/20191223131834-admin-user.js
+++ src/database/seeds/20191223131834-admin-user.js
@@ -7,7 +7,6 @@ module.exports = {
         name: 'Administrador',
         email: 'admin@gympoint.com',
         password_hash: bcrypt.hashSync('123456', 8),
-        provider: true,
         created_at: new Date(),
         updated_at: new Date(),
       },
