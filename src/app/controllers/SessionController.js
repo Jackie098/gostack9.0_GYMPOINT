5e637788823d58ed54bbcad53155ad4f7381d6f5
Jackie098 <carlosmiranda19122@gmail.com>
2019-12-26 22:00:47 -0300

    Corrigido problema no 'UserController': Faltava iniciar a conexão no 'app.js'

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -7,7 +7,7 @@ class SessionController {
   async store(req, res) {
     const { email, password } = req.body;
 
-    const user = User.findOne({ where: { email: req.body.email } });
+    const user = await User.findOne({ where: { email: req.body.email } });
 
     if (!user) {
       return res.status(401).json({ error: 'User not found' });
