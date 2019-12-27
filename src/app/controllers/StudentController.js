9998b5ad53fa7af957c3babaa3083ae7dc505272
Jackie098 <carlosmiranda19122@gmail.com>
2019-12-27 02:24:36 -0300

    Criado 'StudentController' e seu respectivo 'store'

---
+++ src/app/controllers/StudentController.js
@@ -0,0 +1,44 @@
+import * as Yup from 'yup';
+
+import Student from '../models/Student';
+
+class StudentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.number().required(),
+      weight: Yup.number().required(),
+      height: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails ' });
+    }
+
+    const studentExists = await Student.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (studentExists) {
+      return res.status(400).json({ error: 'User already exists.' });
+    }
+
+    const { id, name, email, age, weight, height } = await Student.create(
+      req.body
+    );
+
+    return res.json({
+      id,
+      name,
+      email,
+      age,
+      weight,
+      height,
+    });
+  }
+}
+
+export default new StudentController();
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import StudentController from './app/controllers/StudentController';
 import SessionsController from './app/controllers/SessionController';
 import authMiddleware from './app/middleware/auth';
 
@@ -12,8 +13,13 @@ routes.get('/', (req, res) => res.json({ welcome: 'Hello motherfucks' }));
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionsController.store);
 
-routes.use(authMiddleware);
 // ROTAS QUE PRECISAM DE AUTENTICAÇÃO
+routes.use(authMiddleware);
+
+// SOBRE ADM's
 routes.put('/users', UserController.update);
+
+// SOBRE STUDENTS
+routes.post('/students', StudentController.store);
 
 export default routes;
