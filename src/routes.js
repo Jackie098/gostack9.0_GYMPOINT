787e22ddd96f21e5ff61148325332c1a02cefb3c
Jackie098 <carlosmiranda19122@gmail.com>
2019-12-27 00:18:12 -0300

    Concluído classe de sessão com adm e a respectiva autenticação

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionsController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ welcome: 'Hello motherfucks' }));
 
+// CRIAR NOVO ADM E SESSÃO
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionsController.store);
 
 export default routes;
