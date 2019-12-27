642089c0cf987f14c861428b81d436f0715eaf8f
Jackie098 <carlosmiranda19122@gmail.com>
2019-12-27 01:51:33 -0300

    Criado o model Student

--- src/database/migrations/20191227042552-create-students.js
+++ src/database/migrations/20191227042552-create-students.js
@@ -24,7 +24,7 @@ module.exports = {
         type: Sequelize.DOUBLE,
         allowNull: false,
       },
-      hight: {
+      height: {
         type: Sequelize.DOUBLE,
         allowNull: false,
       },
