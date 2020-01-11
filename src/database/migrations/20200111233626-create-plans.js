fe9e36913659fdc2a8f8d0e5493a5e43dd0b058a
Jackie098 <carlosmiranda19122@gmail.com>
2020-01-11 20:59:08 -0300

    Tabela 'plans' criada

--- src/database/migrations/20200111233626-create-plans.js
+++ src/database/migrations/20200111233626-create-plans.js
@@ -1,6 +1,34 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('plans', { id: Sequelize.INTEGER });
+    return queryInterface.createTable('plans', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      duration: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_ad: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
   },
 
   down: queryInterface => {
