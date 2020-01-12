f853d39944407ff55f56e553d3b2d6d5166c6c5d
Jackie098 <carlosmiranda19122@gmail.com>
2020-01-11 21:06:57 -0300

    Erro ao iniciar o seed

--- src/database/seeds/20200111235941-initials-plans.js
+++ src/database/seeds/20200111235941-initials-plans.js
@@ -1,6 +1,28 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('plans', [{}]);
+  up: queryInterface => {
+    return queryInterface.bulkInsert('plans', [
+      {
+        title: 'Start',
+        duration: 1,
+        price: 129.0,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        title: 'Gold',
+        duration: 3,
+        price: 109.0,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        title: 'Diamond',
+        duration: 6,
+        price: 89.0,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ]);
   },
 
   down: () => {},
