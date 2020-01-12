c75a8787db447daf10faba1d76c917ea143f74b6
Jackie098 <carlosmiranda19122@gmail.com>
2020-01-12 15:36:29 -0300

    Validação de dados de entrada na inserção de um novo plano

--- src/app/controllers/PlanController.js
+++ src/app/controllers/PlanController.js
@@ -1,7 +1,6 @@
-// import * as Yup from 'yup';
+import * as Yup from 'yup';
 
 import Plan from '../models/Plan';
-// import User from '../models/User';
 
 class PlanController {
   async index(req, res) {
@@ -10,6 +9,16 @@ class PlanController {
   }
 
   async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      duration: Yup.number().required(),
+      price: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      res.status(400).json({ error: 'Validation fails' });
+    }
+
     const planExists = await Plan.findOne({
       where: {
         title: req.body.title,
