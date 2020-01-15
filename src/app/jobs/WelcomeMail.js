f6231b5d4edeedeeb577a2ce609681dfd96777bc
Jackie098 <carlosmiranda19122@gmail.com>
2020-01-15 16:55:29 -0300

    Criado 'job' para envio de e-mail (WelcomeMail.js)

---
+++ src/app/jobs/WelcomeMail.js
@@ -0,0 +1,41 @@
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Mail from '../../lib/Mail';
+
+class WelcomeMail {
+  get key() {
+    return 'WelcomeMail';
+  }
+
+  async handle({ data }) {
+    const { enrollment } = data;
+
+    await Mail.sendMail({
+      to: `${enrollment.student.name} <${enrollment.student.email}`,
+      subject: 'Matrícula feita com sucesso',
+      template: 'welcome',
+      context: {
+        student: enrollment.student.name,
+        tot_price: enrollment.price,
+        plan: enrollment.plan.title,
+        duration: enrollment.plan.duration,
+        start_date: format(
+          parseISO(enrollment.start_date),
+          "'dia' dd 'de' MMMM', às' H:mm'h",
+          {
+            locale: pt,
+          }
+        ),
+        end_date: format(
+          parseISO(enrollment.end_date),
+          "'dia' dd 'de' MMMM', às' H:mm'h",
+          {
+            locale: pt,
+          }
+        ),
+      },
+    });
+  }
+}
+
+export default new WelcomeMail();
